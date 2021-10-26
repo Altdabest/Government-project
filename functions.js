6ded800b44db1bd28a3e7ee75f64a169e5a1bfa5
Altdabest <61707822+Altdabest@users.noreply.github.com>
2021-10-26 14:11:06 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,19 @@
-var stuff = 0;
+var c = 1;
+function openNav() {
+    document.getElementById("Menu").style.width = "25%";
+
+    document.getElementById("Menu").style.left = "-10px";
+  }
+  function closeNav() {
+    document.getElementById("Menu").style.width = "0%";
+        // document.getElementById("Menu").style.marginRight= "0%";
+          document.getElementById("Menu").style.left = "-10px";
+        // document.getElementById("Menu").style.display = "block";
+  }
+  function Nav(){
+      (c==1)?openNav():closeNav();
+      c = (c==1)?0:1;
+  }
+  function setC(c){
+    this.c = c;
+  }
